b1e27520a850a1a5c48107ec6828034b9cde2c7d
spiz404 <lorespiz03@gmail.com>
2024-09-30 09:16:12 +0200

    created white board component

---
+++ collaborative-whiteboard/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import WhiteBoardComponent from "./WhiteBoardComponent";
+function App() {
+  return (
+    <div className="whiteboard-container">
+      <WhiteBoardComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ collaborative-whiteboard/src/WhiteBoardComponent.jsx
@@ -0,0 +1,9 @@
+const WhiteBoardComponent = () => {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <canvas id="whiteboard" className="whiteboard-canvas"></canvas>
+    </div>
+  );
+};
+
+export default WhiteBoardComponent;
